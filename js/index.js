d4240705fe62ca8a4429c0deb47e9c7a0eddaa75
RomuloRatis <entreratoseleoes@gmail.com>
2022-06-09 20:16:17 -0300

    Módulo 02

---
+++ js/index.js
@@ -0,0 +1,14 @@
+{
+    let nome = "Rômulo";
+    let numero = 150;
+    let ano = 2022 - numero;
+    let resultado = ano + numero;
+    let quartaFeira = true;
+
+    console.log("O nome é: " + nome);
+    console.log("O número é: " + numero);
+    console.log("O ano é: " + ano);
+    console.log(`O resultado é: ${resultado}`);
+    console.log("Estou mexendo no console.log");
+    console.log(typeof quartaFeira);
+}
